faded35889a6f68099bd77d8112b75fbe116f2fd
syh <syh3237@gmail.com>
2024-06-01 19:49:42 +0900

    0601 - spreadsheet

--- std/JavaScript/Spread-sheet/script.js
+++ std/JavaScript/Spread-sheet/script.js
@@ -7,12 +7,14 @@ const alphabets = [
 ]
 
 class Cell {
-    constructor(isHeader, disabled, data, row, column, active = false) {
+    constructor(isHeader, disabled, data, row, column, rowName, columnName, active = false) {
         this.isHeader = isHeader;
         this.disabled = disabled;
         this.data = data;
         this.row = row;
         this.column = column;
+        this.rowName = rowName;
+        this.columnName = columnName; 
         this.active = active;
     }
 }
@@ -24,13 +26,18 @@ function initSpreadsheet() {
         let spreadsheetRow = [];
         for (let j = 0; j < COLS; j++) {
             let cellData = '';
-            
+            let isHeader = false;
+            let disabled = false;
             // 모든 row 첫 번째 컬럼에 숫자 넣기
             if(j === 0) {
                 cellData = i;
+                isHeader = true;
+                disabled = true;
             }
             if(i === 0) {
                 cellData = alphabets[j - 1];
+                isHeader = true;
+                disabled = true;
             }
 
             // 첫 번째 row의 컬럼은 '';
@@ -38,7 +45,10 @@ function initSpreadsheet() {
                 cellData = '';
             }
 
-            const cell = new Cell(false,false, cellData, i, j, false);
+            const rowName = i;
+            const columnName = alphabets[j - 1];
+
+            const cell = new Cell(isHeader ,disabled  , cellData, i, j, rowName, columnName,  false);
             spreadsheetRow.push(cell);
         }
         spreadsheet.push(spreadsheetRow);
@@ -53,7 +63,37 @@ function createCellEl(cell) {
     cellEl.id = 'cell_' + cell.row + cell.column;
     cellEl.value = cell.data;
     cellEl.disabled = cell.disabled;
+
+    if(cell.isHeader) {
+        cellEl.classList.add('header');
+    }
+
+    cellEl.onclick = () => handleCellClick(cell);
+
     return cellEl;
+}
+
+function handleCellClick(cell ) {
+    const columnHeader = spreadsheet[cell.row][0];
+    const rowHeader = spreadsheet[0][cell.column];
+    const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+    const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+    columnHeaderEl.classList.add('active');
+    rowHeaderEl.classList.add('active'); 
+
+    console.log('clicked',columnHeaderEl, rowHeaderEl) ; 
+}
+
+function clearHeaderActiveStates() {
+    const headers = document.querySelector('.header');
+
+    headers.forEach((header) => {
+        header.classList.remove('active');
+    });
+}
+
+function getElFromRowCol(row, col) {
+    return document.querySelector('#cell_' + row + col)
 }
 
 function drawSheet() {
