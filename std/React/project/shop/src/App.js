661c57ed77ec9a79cef1834a6a1e4805b6430494
hun <syh3237@naver.com>
2024-06-23 23:16:52 +0900

    react-shop

--- std/React/project/shop/src/App.js
+++ std/React/project/shop/src/App.js
@@ -3,11 +3,13 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './pages/detail.js';
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -17,8 +19,8 @@ function App() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="/">홈</Nav.Link>
-                <Nav.Link href="/detail">상세페이지</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link onClick={()=> {navigate('/detail')}}>Cart</Nav.Link>
                 <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
@@ -41,6 +43,8 @@ function App() {
           </Container>
         </Navbar>
 
+
+
         <Routes>
           <Route path='/' element={
             <>
@@ -58,12 +62,38 @@ function App() {
               </div>
             </>
             } />
-          <Route path='detail' element={<div>상세 페이지</div>} />
+          <Route path='/detail' element={ <Detail shoes={shoes}/> } />
+
+          <Route path='/about' element={ <About/> }>
+            <Route path='member' element={ <div>멤버</div> } />
+            <Route path='location' element={ <div>위치 정보</div> } />
+          </Route>
+
+          <Route path='/event' element={ <Event/> }>
+            <Route path='one' element={<p>첫 주문시 양배추즙 서비스</p>}/>
+            <Route path='two' element={<p>생일기념 쿠폰 받기</p>}/>
+          </Route>
         </Routes>
-
-
     </div>
   );
+}
+
+function Event(){
+  return(
+    <div>
+      <h3>오늘의 이벤트</h3>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function About() {
+  return(
+    <div>
+      <h3>회사 정보</h3>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props) {
---
+++ std/React/project/shop/src/pages/detail.js
@@ -0,0 +1,19 @@
+function Detail(props) {
+    return(
+        <div className="container">
+            <div className="row">
+            <div className="col-md-6">
+                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            </div>
+            <div className="col-md-6">
+                <h4 className="pt-5">{props.shoes[0].title}</h4>
+                <p>{props.shoes[0].content}</p>
+                <p>{props.shoes[0].price}원</p>
+                <button className="btn btn-danger">주문하기</button> 
+            </div>
+            </div>
+        </div> 
+    )
+}
+
+export default Detail;
