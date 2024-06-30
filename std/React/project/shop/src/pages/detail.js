ba70a1e635ca7f024d9feb9ff0dd5de13409ce22
hun <syh3237@naver.com>
2024-06-30 23:40:07 +0900

    shop

--- std/React/project/shop/src/pages/detail.js
+++ std/React/project/shop/src/pages/detail.js
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+
 
 function Detail(props) {
 
-    // let {id} = useParams();
+    useEffect(()=>{
+        console.log('HI')
+    })
+
+    let [count, setCount] = useState(0)
+    
+    let {id} = useParams();
+    let 찾은상품 = props.shoes.find((x)=> x.id == id)
 
     return(
         <div className="container">
+            <div className="alert alert-warning">2초이내 구매 시 할인</div>
+            <button onClick={()=>{ setCount(count+1) }}>버튼</button>
             <div className="row">
             <div className="col-md-6">
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             </div>
             <div className="col-md-6">
-                <h4 className="pt-5">{props.shoes[0].title}</h4>
-                <p>{props.shoes[0].content}</p>
-                <p>{props.shoes[0].price}원</p>
+                <h4 className="pt-5">{찾은상품.title}</h4>
+                <p>{찾은상품.cotent}</p>
+                <p>{찾은상품.price}원</p>
                 <button className="btn btn-danger">주문하기</button> 
             </div>
             </div>
